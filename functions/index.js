5d51b725108e7c1bd1b525d177cd2856d2dd30b0
Jose Antonini <jocantonini@gmail.com>
2017-10-02 02:37:30 -0300

    Initial Commit

---
+++ functions/index.js
@@ -0,0 +1,28 @@
+const firebase = require("firebase-admin");
+const functions = require('firebase-functions');
+const express = require("express");
+const engines = require("consolidate");
+
+const firebaseApp = firebase.initializeApp(
+    functions.config().firebase
+);
+
+function getFacts() {
+    const ref = firebase.database().ref("lugares");
+    return ref.once("value").then(snap => snap.val());
+}
+
+const app = express();
+
+app.engine("hbs", engines.handlebars);
+app.set("views", "./views");
+app.set("view engine", "hbs");
+
+
+app.get("/", (req, res) => {
+    getFacts().then(lugares => {
+        res.render("index", { lugares });
+    });
+});
+
+exports.app = functions.https.onRequest(app);
