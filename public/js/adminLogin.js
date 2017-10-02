f840a0c66a1c78f350f1a5d8fdef6c1f559567d6
unknown <jocantonini@gmail.com>
2017-10-02 16:22:11 -0300

    Aded public adminLogin and admin

---
+++ public/js/adminLogin.js
@@ -0,0 +1,10 @@
+(function(){
+    var config = {
+        apiKey:"AIzaSyD_FuI6e9zDwsKtzJAgy9OsYsswPjPB6NU",
+        authDomain: "el-bondi-server.firebaseapp.com",
+        databaseURL: "https://el-bondi-server.firebaseio.com"
+        /*,storageBucket: "<BUCKET>.appspot.com",
+        messagingSenderId: "<SENDER_ID>",*/
+      };
+      firebase.initializeApp(config);
+}());
