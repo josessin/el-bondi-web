c40c7bcc1e8c686837adc8b6756565e1194cb276
Jose Antonini <jocantonini@gmail.com>
2017-10-09 20:17:19 -0300

    boton de eliminar, validar fechas de ingreso

--- functions/statics/js/admin.js
+++ functions/statics/js/admin.js
@@ -14,25 +14,29 @@ var __entradas;
 }());
 
 
-function guardar(entrada) {
+function guardar(jEntrada) {
 
-    entrada.find(".btnGuaradar").text("Conectando").prop("disabled", true);
+    if (!validarFecha(jEntrada)) {
+        alert("La fecha no ha sido ingresada correctamente o es anterior a la fecha de hoy!\n\
+        Los cambios no se guardaron.");
+        return;
+    }
 
-
+    jEntrada.find(".btnGuaradar").text("Conectando").prop("disabled", true);
     var db = firebase.database();
-    var ule = entrada.find(".ulentrada");
+    var ule = jEntrada.find(".ulentrada");
 
-
-    console.log(ule[0].id);
+    console.log("Id: " + ule[0].id);
+    console.log("Fecha: " + jEntrada.find(".inFecha")[0].value);
 
     db.ref("/locaciones/" + ule[0].id).set({
 
-        fecha: entrada.find(".inFecha")[0].value,
-        direccion: entrada.find(".inDireccion")[0].value,
-        nota: entrada.find(".inNota")[0].value
+        fecha: jEntrada.find(".inFecha")[0].value,
+        direccion: jEntrada.find(".inDireccion")[0].value,
+        nota: jEntrada.find(".inNota")[0].value
 
     }).then(function () {
-        entrada.find(".btnGuaradar").prop("disabled", false).addClass("hide").text("Guardar");
+        jEntrada.find(".btnGuaradar").prop("disabled", false).addClass("hide").text("Guardar");
     }).catch(function () {
         window.location.replace(window.location.href);
     });
@@ -47,8 +51,19 @@ function agregarEntrada() {
     registrarCambios(nuEl[0]);
 }
 
-function registrarCambios(entrada) {
+function borrarEntrada(entrada) {
+    var db = firebase.database();
+    var key = $(entrada).find(".ulentrada").prop("id");
+    db.ref("/locaciones/" + key).remove().then(function () {
+        $(entrada).remove();
+    }).catch(function (e) {
+        alert("No se puede conectar con la base de datos,\
+        puede que no estes conectado a internet o tu sesión se haya cerrado.\n\
+        Intenta refrescar la pagina");
+    });
+}
 
+function registrarCambios(entrada) {
 
     $(entrada).find(".inFecha").on("change paste keyup", function () {
         $(entrada).find(".btnGuaradar").removeClass("hide");
@@ -61,34 +76,57 @@ function registrarCambios(entrada) {
     $(entrada).find(".inNota").on("change paste keyup", function () {
         $(entrada).find(".btnGuaradar").removeClass("hide");
     });
+    
+    if(validarFecha($(entrada))){
+        $(entrada).find(".btnBorrar").removeClass("hide");
+        $(entrada).find("input").prop("disabled", false);
+    }
 }
 
-function logout(){
-    firebase.auth().signOut().then(function(){
+function logout() {
+    firebase.auth().signOut().then(function () {
         window.location.replace(window.location.href);
     });
+}
+
+function validarFecha(jEntrada) {
+    var selectedText = jEntrada.find(".inFecha").prop("value");
+    if(!selectedText){
+        return false;
+    }
+    var selectedDate = new Date(selectedText);
+    var now = new Date();
+    now.setDate(now.getDate()-1);
+    if (selectedDate < now){
+        return false;
+    }
+    return true;
+
 }
 
 var entradaElemnt = '\
 <div class="entrada">\
     <ul class="ulentrada">\
         <li class="ent">\
+            <button class="btnBorrar hide" type="button" onclick="borrarEntrada($(this).parent().closest(\'div\'))">X</button>\
+        </li>\
+        <li class="ent">\
             <p>Fecha</p>\
         </li>\
         <li class="ent">\
-            <input class="inFecha" type="date" >\
+            <input class="inFecha" type="date" disabled >\
         </li>\
         <li class="ent">\
             <p>Dir</p>\
         </li>\
         <li class="ent">\
-            <input class="inDireccion" type="text">\
+            <input class="inDireccion" type="text" disabled>\
         </li>\
         <li class="ent">\
             <p>Info</p>\
         </li>\
         <li class="ent">\
-            <input class="inNota" type="text" >\
+            <input class="inNota" type="text" disabled>\
         </li>\
         <li class="ent">\
             <button class="btnGuaradar hide" type="button" onclick="guardar($(this).parent().closest(\'div\'))">Guardar</button>\
