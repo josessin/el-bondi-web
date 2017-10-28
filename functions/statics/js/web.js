2f4298630e694538dc81f04abfcb287f9f08a068
Jose Antonini <jocantonini@gmail.com>
2017-10-28 19:58:40 -0300

    api de google maps para la web

---
+++ functions/statics/js/web.js
@@ -0,0 +1,34 @@
+
+function init() {
+
+    var url = window.location.href.split("/")[0];
+
+    $.getJSON(url + "app/loc?cantidad=1", function (data) {
+        
+        var dir = data.entradas[0].direccion
+        $("#dirActual").html(dir);
+        var geocoder = new google.maps.Geocoder();
+
+        var mendoza = { lat: -32.8895, lng: 68.8458 };
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 15,
+            center: mendoza
+        });
+
+        var geocoder = new google.maps.Geocoder();
+
+        geocoder.geocode({"address": dir},function(results,status){
+            if(status === "OK"){
+                map.setCenter(results[0].geometry.location);
+                var marker = new google.maps.Marker({
+                    position: results[0].geometry.location,
+                    map: map
+                });
+            }else{
+                alert("Geocode no fue exitoso por la siguiente razon: " + status);
+            }
+        });
+       
+    })
+
+}
